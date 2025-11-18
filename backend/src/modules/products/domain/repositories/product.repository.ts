abbdf32a5b552e765ab2f016b8a718bdb79bc942
Vinícius <buenov535@gmail.com>
2025-11-18 08:50:23 -0300

    feat<products>: repository must have a find by slug method, renamed create method

--- backend/src/modules/products/domain/repositories/product.repository.ts
+++ backend/src/modules/products/domain/repositories/product.repository.ts
@@ -1,9 +1,10 @@
 import { ProductEntity } from "../entities/product.entity";
 
 export interface ProductRepository {
-    create(product: ProductEntity): Promise<void>;
+    save(product: ProductEntity): Promise<ProductEntity>;
     delete(id: string): Promise<void>;
     update(product: ProductEntity): Promise<void>;
     findById(id: string): Promise<ProductEntity | null>;
+    findBySlug(slug: string): Promise<ProductEntity | null>;
     findAll(query: string): Promise<ProductEntity[]>;
 }
