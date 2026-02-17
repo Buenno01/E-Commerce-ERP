acc3304e90b2eeceed4f417126114b975dec3ac3
Vinícius <buenov535@gmail.com>
2026-02-17 18:29:20 -0300

    feat: added product attributes type to product type schema

--- backend/src/modules/products/infra/graphql/schemas/product.type.ts
+++ backend/src/modules/products/infra/graphql/schemas/product.type.ts
@@ -1,33 +1,68 @@
-import { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import {
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
 import { TimestampScalar } from "@/infra/graphql/scalars/timestamp.scalar";
 import { Money } from "@/infra/graphql/schema/money.type";
 
 export const ProductType = new GraphQLObjectType({
-    name: "Product",
-    fields: {
-        id: {
-            type: new GraphQLNonNull(GraphQLID)
-        },
-        sku: {
-            type: GraphQLString
-        },
-        name: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-        slug: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-        description: {
-            type: GraphQLString
-        },
-        price: {
-            type: Money
-        },
-        createdAt: {
-            type: TimestampScalar
-        },
-        updatedAt: {
-            type: TimestampScalar
-        }
-    }
+  name: "Product",
+  fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+    },
+    sku: {
+      type: GraphQLString,
+    },
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    slug: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    description: {
+      type: GraphQLString,
+    },
+    price: {
+      type: Money,
+    },
+    createdAt: {
+      type: TimestampScalar,
+    },
+    updatedAt: {
+      type: TimestampScalar,
+    },
+  },
+});
+
+export const ProductAttributes = new GraphQLObjectType({
+  name: "ProductAttributes",
+  fields: {
+    id: {
+      type: GraphQLID,
+    },
+    sku: {
+      type: GraphQLString,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    slug: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    price: {
+      type: Money,
+    },
+    createdAt: {
+      type: TimestampScalar,
+    },
+    updatedAt: {
+      type: TimestampScalar,
+    },
+  },
 });
