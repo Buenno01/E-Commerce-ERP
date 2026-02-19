13d35ccfa516a35a431407570b773f33b3067601
Vinícius <buenov535@gmail.com>
2026-02-19 08:52:52 -0300

    refactor: adjusted product entity and constructor

--- backend/src/modules/products/application/use-cases/product-create.use-case.ts
+++ backend/src/modules/products/application/use-cases/product-create.use-case.ts
@@ -1,43 +1,42 @@
-import { ProductEntity } from "../../domain/entities/product.entity";
-import { ProductRepository } from "../../domain/repositories/product.repository";
-import { MoneyVO } from "../../domain/value-objects/money.vo";
-import { ProductNameVO } from "../../domain/value-objects/product-name.vo";
-import { SkuVO } from "../../domain/value-objects/sku.vo";
-import { SlugVO } from "../../domain/value-objects/slug.vo";
+import { ProductEntity } from "@products/domain/entities/product.entity";
+import { ProductRepositoryInterface } from "@products/domain/repositories/product.repository";
+import { MoneyVO } from "@products/domain/value-objects/money.vo";
+import { ProductNameVO } from "@products/domain/value-objects/product-name.vo";
+import { SkuVO } from "@products/domain/value-objects/sku.vo";
+import { SlugVO } from "@products/domain/value-objects/slug.vo";
 import { CreateProductDTO } from "../dtos/product-create.dto";
 
 export class ProductCreateUseCase {
-    constructor(
-        private readonly productRepository: ProductRepository,
-    ) {}
+  constructor(private readonly productRepository: ProductRepositoryInterface) {}
 
-    async execute(product: CreateProductDTO): Promise<void> {
-        const targetSlug = product.slug ? product.slug : product.name;
-        const slug = await this.createSlug(targetSlug);
-        const productEntity = new ProductEntity(
-            crypto.randomUUID(),
-            new ProductNameVO(product.name),
-            slug,
-            product.description ?? '',
-            new MoneyVO(product.price.amount, product.price.currencyCode),
-            product.quantity ?? 0,
-            new Date(),
-            new Date(),
-            new SkuVO(product.sku ?? ''),
-        );
-        await this.productRepository.save(productEntity);
+  async execute(product: CreateProductDTO): Promise<void> {
+    const targetSlug = product.slug ? product.slug : product.name;
+
+    const productEntity = ProductEntity.create({
+      id: crypto.randomUUID(),
+      name: new ProductNameVO(product.name),
+      slug: await this.createSlug(targetSlug),
+      description: product.description ?? null,
+      price: new MoneyVO(product.price.amount, product.price.currencyCode),
+      quantity: product.quantity ?? 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      sku: new SkuVO(product.sku ?? ""),
+    });
+
+    await this.productRepository.save(productEntity);
+  }
+
+  private async createSlug(value: string): Promise<SlugVO> {
+    const baseSlug = SlugVO.fromString(value);
+    let slug = baseSlug;
+    let i = 1;
+
+    while (await this.productRepository.findBySlug(slug.value)) {
+      slug = new SlugVO(`${baseSlug.value}-${i}`);
+      i++;
     }
 
-    private async createSlug(value: string): Promise<SlugVO> {
-        const baseSlug = SlugVO.fromString(value);
-        let slug = baseSlug;
-        let i = 1;
-
-        while (await this.productRepository.findBySlug(slug.value)) {
-            slug = new SlugVO(`${baseSlug.value}-${i}`);
-            i++;
-        }
-
-        return slug;
-    }
+    return slug;
+  }
 }
--- backend/src/modules/products/domain/entities/product.entity.ts
+++ backend/src/modules/products/domain/entities/product.entity.ts
@@ -3,16 +3,70 @@ import { ProductNameVO } from "../value-objects/product-name.vo";
 import { SkuVO } from "../value-objects/sku.vo";
 import { SlugVO } from "../value-objects/slug.vo";
 
+interface ProductProps {
+  id: string;
+  name: ProductNameVO;
+  slug: SlugVO;
+  description: string | null;
+  price: MoneyVO;
+  quantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+  sku: SkuVO;
+}
+
 export class ProductEntity {
-    constructor(
-        public readonly id: string,
-        public name: ProductNameVO,
-        public slug: SlugVO,
-        public description: string,
-        public price: MoneyVO,
-        public quantity: number,
-        public createdAt: Date,
-        public updatedAt: Date,
-        public sku: SkuVO,
-    ) {}
+  private readonly _id: string;
+  private _name: ProductNameVO;
+  private _slug: SlugVO;
+  private _description: string | null;
+  private _price: MoneyVO;
+  private _quantity: number;
+  private _createdAt: Date;
+  private _updatedAt: Date;
+  private _sku: SkuVO;
+
+  private constructor(private props: ProductProps) {
+    this._id = this.props.id;
+    this._name = this.props.name;
+    this._slug = this.props.slug;
+    this._description = this.props.description;
+    this._price = this.props.price;
+    this._quantity = this.props.quantity;
+    this._createdAt = this.props.createdAt;
+    this._updatedAt = this.props.updatedAt;
+    this._sku = this.props.sku;
+  }
+
+  static create(props: ProductProps): ProductEntity {
+    return new ProductEntity(props);
+  }
+
+  get id() {
+    return this._id;
+  }
+  get name() {
+    return this._name;
+  }
+  get slug() {
+    return this._slug;
+  }
+  get price() {
+    return this._price;
+  }
+  get sku() {
+    return this._sku;
+  }
+  get description() {
+    return this._description;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get createdAt() {
+    return this._createdAt;
+  }
+  get updatedAt() {
+    return this._updatedAt;
+  }
 }
