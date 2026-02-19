c494c8ba242a5b6ce35bad974cbb605738bcd887
Vinícius <buenov535@gmail.com>
2026-02-19 18:15:44 -0300

    refactor: changed product create input file name

---
+++ backend/src/modules/products/infra/graphql/schemas/product-create-input.type.ts
@@ -0,0 +1,31 @@
+import { MoneyInput } from "@/infra/graphql/schema/money-input.type";
+import {
+  GraphQLInputObjectType,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLString,
+} from "graphql";
+
+export const ProductCreateInput = new GraphQLInputObjectType({
+  name: "ProductCreateInput",
+  fields: {
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    slug: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    price: {
+      type: new GraphQLNonNull(MoneyInput),
+    },
+    quantity: {
+      type: GraphQLInt,
+    },
+    sku: {
+      type: GraphQLString,
+    },
+  },
+});
