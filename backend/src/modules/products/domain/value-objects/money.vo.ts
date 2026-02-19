90eb21497947cc2c70f38ad9472e97aaf4d2ff91
Vinícius <buenov535@gmail.com>
2026-02-19 08:54:55 -0300

    refactor: made the error message better

--- backend/src/modules/products/domain/value-objects/money.vo.ts
+++ backend/src/modules/products/domain/value-objects/money.vo.ts
@@ -1,13 +1,13 @@
 export class MoneyVO {
-    constructor(
-        public readonly amount: number,
-        public readonly currency: string,
-    ) {
-        if (amount < 0) {
-            throw new Error('Amount must be greater than 0');
-        }
-        if (currency.length !== 3 || !currency.match(/^[A-Z]{3}$/)) {
-            throw new Error('Currency must be a valid currency code');
-        }
+  constructor(
+    public readonly amount: number,
+    public readonly currency: string,
+  ) {
+    if (amount < 0) {
+      throw new Error("Amount must be greater than or equal to 0");
     }
+    if (currency.length !== 3 || !currency.match(/^[A-Z]{3}$/)) {
+      throw new Error("Currency must be a valid currency code");
+    }
+  }
 }
