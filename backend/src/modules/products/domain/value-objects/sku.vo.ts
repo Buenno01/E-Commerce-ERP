33f97069770fd07d10d33da1399a56a0028f0dbb
Vinícius <buenov535@gmail.com>
2026-02-20 08:36:24 -0300

    fix: adjusted sku validations

--- backend/src/modules/products/domain/value-objects/sku.vo.ts
+++ backend/src/modules/products/domain/value-objects/sku.vo.ts
@@ -1,9 +1,10 @@
 export class SkuVO {
-    constructor(
-        public readonly value: string,
-    ) {
-        if (!value.match(/^[a-zA-Z0-9]+$/)) {
-            throw new Error('SKU must contain only letters and numbers');
-        }
+  constructor(public readonly value: string) {
+    if (typeof value !== "string") {
+      throw new Error("SKU must be a valid string");
     }
+    if (!value.match(/^[a-zA-Z0-9-]+$/)) {
+      throw new Error("SKU must contain only letters, numbers and hyphen");
+    }
+  }
 }
