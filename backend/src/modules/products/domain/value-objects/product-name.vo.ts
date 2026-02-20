5741e33f68b4f7caf8ee56b0e0cc4cdca61e73bd
Vinícius <buenov535@gmail.com>
2026-02-20 08:24:57 -0300

    fix: implemented type check on product name

--- backend/src/modules/products/domain/value-objects/product-name.vo.ts
+++ backend/src/modules/products/domain/value-objects/product-name.vo.ts
@@ -1,9 +1,10 @@
 export class ProductNameVO {
-    constructor(
-        public readonly value: string,
-    ) {
-        if (value.length < 3 || value.length > 255) {
-            throw new Error('Product name must be between 3 and 255 characters');
-        }
+  constructor(public readonly value: string) {
+    if (typeof value !== "string") {
+      throw new Error("Product name must be a valid string");
     }
+    if (value.length < 3 || value.length > 255) {
+      throw new Error("Product name must be between 3 and 255 characters");
+    }
+  }
 }
