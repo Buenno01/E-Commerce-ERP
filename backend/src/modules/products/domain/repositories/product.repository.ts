783a55fb61816b870feb84346a72ac06e4aef9db
Vinícius <buenov535@gmail.com>
2026-02-24 22:51:27 -0300

    feat: updated findAll args type

--- backend/src/modules/products/domain/repositories/product.repository.ts
+++ backend/src/modules/products/domain/repositories/product.repository.ts
@@ -1,10 +1,23 @@
 import { ProductEntity } from "../entities/product.entity";
-
+export type ProductFilter = {
+  name?: { equals?: string; contains?: string; startsWith?: string };
+  sku?: { equals?: string; contains?: string; startsWith?: string };
+  price?: {
+    equals?: number;
+    gte?: number;
+    lte?: number;
+    gt?: number;
+    lt?: number;
+  };
+  AND?: ProductFilter[];
+  OR?: ProductFilter[];
+  NOT?: ProductFilter;
+};
 export interface ProductRepositoryInterface {
   save(product: ProductEntity): Promise<void>;
   delete(id: string): Promise<void>;
   update(product: ProductEntity): Promise<void>;
   findById(id: string): Promise<ProductEntity | null>;
   findBySlug(slug: string): Promise<ProductEntity | null>;
-  findAll(query: string): Promise<ProductEntity[]>;
+  findAll(filters: ProductFilter): Promise<ProductEntity[]>;
 }
