2e58dd57809332af001bd010bf0f415b1668e928
Vinícius <buenov535@gmail.com>
2026-02-25 22:55:38 -0300

    feat: added pagination params to gql resolver

--- backend/src/modules/products/infra/graphql/resolvers/index.ts
+++ backend/src/modules/products/infra/graphql/resolvers/index.ts
@@ -3,7 +3,7 @@ import productById from "./product-by-id.resolver";
 import productsQuery from "./products-query.resolver";
 import { ProductType } from "../schemas/product.type";
 import { ProductCreateInput } from "../schemas/product-create-input.type";
-import { GraphQLID, GraphQLNonNull } from "graphql";
+import { GraphQLID, GraphQLInt, GraphQLNonNull, GraphQLString } from "graphql";
 import { ProductConnection } from "../schemas/product-connection.type";
 import { ProductFilterType } from "../schemas/product-connection-filter.type";
 
@@ -33,6 +33,21 @@ export const Query = {
     args: {
       filters: {
         type: ProductFilterType,
+      },
+      after: {
+        type: GraphQLString,
+        description:
+          "Cursor for forward pagination: returns products listed after this cursor. (Takes precedence if 'before' is also defined)",
+      },
+      before: {
+        type: GraphQLString,
+        description:
+          "Cursor for backward pagination: returns products listed before this cursor.",
+      },
+      first: {
+        type: GraphQLInt,
+        description: "The amount of items that should be returned",
+        default: 15,
       },
     },
   },
--- backend/src/modules/products/infra/graphql/resolvers/products-query.resolver.ts
+++ backend/src/modules/products/infra/graphql/resolvers/products-query.resolver.ts
@@ -1,7 +1,11 @@
 import { ProductsQueryUseCase } from "@/modules/products/application/use-cases/products-query.use-case";
 
-export default function productsQuery(_: any, { filters }: any, context: any) {
+export default function productsQuery(
+  _: any,
+  { filters, first, before, after }: any,
+  context: any,
+) {
   const useCase = context.container.resolve(ProductsQueryUseCase);
 
-  return useCase.execute(filters);
+  return useCase.execute(filters, first, before, after);
 }
