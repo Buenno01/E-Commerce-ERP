0bf8481eb38a3e1bdcfa27c16979d3413d963bba
Vinícius <buenov535@gmail.com>
2026-02-25 22:55:15 -0300

    feat: implemented pagination logic to products query use case

--- backend/src/modules/products/application/use-cases/products-query.use-case.ts
+++ backend/src/modules/products/application/use-cases/products-query.use-case.ts
@@ -4,19 +4,43 @@ import {
 } from "@products/domain/repositories/product.repository";
 import { ProductEntity } from "../../domain/entities/product.entity";
 import { ConnectionUseCaseInterface } from "@/infra/graphql/schema/connection.type";
+import * as cursor from "@/lib/cursor";
 
 type ProductConnectionType = ConnectionUseCaseInterface<ProductEntity>;
 
 export class ProductsQueryUseCase {
   constructor(private readonly productRepository: ProductRepositoryInterface) {}
 
-  async execute(filters: ProductFilter): Promise<ProductConnectionType> {
-    const nodes = await this.productRepository.findAll(filters);
+  async execute(
+    filters: ProductFilter,
+    first: number,
+    before: string,
+    after: string,
+  ): Promise<ProductConnectionType> {
+    const hasAnyCursor = after || before;
+    const fromProductId = hasAnyCursor
+      ? cursor.decode(after || before)
+      : undefined;
+
+    const takeIsPositive = (!after && !before) || !!after;
+    const take = takeIsPositive ? first : -1 * first;
+
+    const nodes = await this.productRepository.findAll(
+      take > 0 ? take + 2 : take - 2,
+      filters,
+      fromProductId,
+    );
+
+    nodes.shift();
+
+    const hasExtraItem = nodes.length > first;
+
+    if (hasExtraItem) nodes.pop();
 
     return {
       nodes,
-      hasNextPage: false,
-      hasPreviousPage: false,
+      hasNextPage: takeIsPositive ? hasExtraItem : false,
+      hasPreviousPage: !takeIsPositive ? hasExtraItem : false,
     };
   }
 }
