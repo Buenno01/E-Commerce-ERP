a5d81277bd9f6b9625a2008982b93c17f235d202
Vinícius <buenov535@gmail.com>
2026-03-03 18:45:04 -0300

    feat(shops): created new shop entity

---
+++ backend/src/modules/shops/domain/entities/shop.entity.ts
@@ -0,0 +1,46 @@
+import { NameVO } from "@/shared/domain/value-objects/name.vo";
+import { SlugVO } from "@/shared/domain/value-objects/slug.vo";
+
+interface ShopProps {
+  id: string;
+  name: NameVO;
+  slug: SlugVO;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class ShopEntity {
+  private readonly _id: string;
+  private _name: NameVO;
+  private _slug: SlugVO;
+  private _createdAt: Date;
+  private _updatedAt: Date;
+
+  private constructor(private props: ShopProps) {
+    this._id = this.props.id;
+    this._name = this.props.name;
+    this._slug = this.props.slug;
+    this._createdAt = this.props.createdAt;
+    this._updatedAt = this.props.updatedAt;
+  }
+
+  static create(props: ShopProps): ShopEntity {
+    return new ShopEntity(props);
+  }
+
+  get id() {
+    return this._id;
+  }
+  get name() {
+    return this._name;
+  }
+  get slug() {
+    return this._slug;
+  }
+  get createdAt() {
+    return this._createdAt;
+  }
+  get updatedAt() {
+    return this._updatedAt;
+  }
+}
