b31b3e8efedbcfad24e0a7b83f7784574147e6a4
Vinícius <buenov535@gmail.com>
2026-03-02 22:19:25 -0300

    feat(users): implemented user create use case

---
+++ backend/src/modules/users/application/use-cases/user-create.use-case.ts
@@ -0,0 +1,26 @@
+import { UserEntity } from "../../domain/entities/user.entity";
+import { UserRepository } from "../../domain/repositories/user.repository";
+import { EmailVO } from "../../domain/value-objects/email.vo";
+import { NameVO } from "../../domain/value-objects/name.vo";
+import { UserCreateDTO } from "../dtos/user-create.dto";
+
+export class UserCreateUseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute(user: UserCreateDTO): Promise<UserEntity> {
+    const date = new Date();
+
+    const userEntity = UserEntity.create({
+      id: crypto.randomUUID(),
+      email: new EmailVO(user.email),
+      firstName: new NameVO(user.firstName),
+      lastName: new NameVO(user.lastName),
+      createdAt: date,
+      updatedAt: date,
+    });
+
+    await this.userRepository.save(userEntity);
+
+    return userEntity;
+  }
+}
