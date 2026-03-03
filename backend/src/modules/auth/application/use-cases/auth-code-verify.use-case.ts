21ea9192a161ccc48c5439a45565f6661335b3e7
Vinícius <buenov535@gmail.com>
2026-03-03 08:32:19 -0300

    feat(auth): implemented verify code use case

--- backend/src/modules/auth/application/use-cases/auth-code-verify.use-case.ts
+++ backend/src/modules/auth/application/use-cases/auth-code-verify.use-case.ts
@@ -1,31 +1,67 @@
+import * as jwt from "jsonwebtoken";
+
+import { UserRepository } from "@/modules/users/domain/repositories/user.repository";
 import { AuthCodeRepository } from "../../domain/repositories/auth-code.repository";
 import { AuthCodeVerifyDTO } from "../dtos/auth-code-verify.dto";
+import { UserEntity } from "@/modules/users/domain/entities/user.entity";
+import { EmailVO } from "@/shared/domain/value-objects/email.vo";
 
-const FIVE_MINUTES = 300000;
+const FIVE_MINUTES = 5 * 60 * 1000;
 
-export class AuthCodeRequestUseCase {
-  constructor(private readonly authCodeRepository: AuthCodeRepository) {}
+export class AuthCodeVerifyUseCase {
+  constructor(
+    private readonly authCodeRepository: AuthCodeRepository,
+    private readonly userRepository: UserRepository,
+  ) {}
 
-  async execute(props: AuthCodeVerifyDTO): Promise<{ token: string }> {
+  async execute({
+    email,
+    code,
+  }: AuthCodeVerifyDTO): Promise<{ token: string }> {
+    const emailVO = new EmailVO(email);
+
     const authCodeEntity = await this.authCodeRepository.findByEmail(
-      props.email,
+      emailVO.value,
     );
+
+    if (!authCodeEntity) {
+      throw new Error("This code is not valid or has expired.");
+    }
 
     const now = new Date();
 
     if (
-      authCodeEntity.code.value !== props.code ||
+      authCodeEntity.code.value !== code ||
       now.getTime() - authCodeEntity.createdAt.getTime() > FIVE_MINUTES
     ) {
+      await this.authCodeRepository.deleteByEmail(emailVO.value);
       throw new Error("This code is not valid or has expired.");
     }
 
-    // TODO: Create user if they don't exist
+    await this.authCodeRepository.deleteByEmail(emailVO.value);
 
-    // TODO: generate token with user id and email
+    const existingUser = await this.userRepository.findByEmail(emailVO.value);
+
+    let userEntity = existingUser as UserEntity;
+
+    if (!existingUser) {
+      userEntity = UserEntity.create({
+        email: emailVO,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        id: crypto.randomUUID(),
+      });
+
+      await this.userRepository.save(userEntity);
+    }
+
+    const token = jwt.sign(
+      { sub: userEntity.id, email: userEntity.email.value },
+      process.env.JWT_SECRET!,
+    );
 
     return {
-      token: Buffer.from("Not implemented!").toString("base64"),
+      token,
     };
   }
 }
