1fdcea57076e9c5986c0ff545d410d0462f1262e
Vinícius <buenov535@gmail.com>
2026-03-02 23:45:49 -0300

    fix(auth): adusted auth code repository to return the code entity correctly

--- backend/src/modules/auth/domain/repositories/auth-code.repository.ts
+++ backend/src/modules/auth/domain/repositories/auth-code.repository.ts
@@ -2,5 +2,5 @@ import { AuthCodeEntity } from "../entities/auth-code.entity";
 
 export interface AuthCodeRepository {
   save(authCode: AuthCodeEntity): Promise<void>;
-  findByEmail(email: string): Promise<void>;
+  findByEmail(email: string): Promise<AuthCodeEntity>;
 }
