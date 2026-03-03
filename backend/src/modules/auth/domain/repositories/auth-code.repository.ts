4a71779a63d1d9255dcef93b8916ff0b38206b8a
Vinícius <buenov535@gmail.com>
2026-03-03 00:07:37 -0300

    feat(auth): added delete by email signature to auth code repository interface

--- backend/src/modules/auth/domain/repositories/auth-code.repository.ts
+++ backend/src/modules/auth/domain/repositories/auth-code.repository.ts
@@ -3,4 +3,5 @@ import { AuthCodeEntity } from "../entities/auth-code.entity";
 export interface AuthCodeRepository {
   save(authCode: AuthCodeEntity): Promise<void>;
   findByEmail(email: string): Promise<AuthCodeEntity>;
+  deleteByEmail(email: string): Promise<void>;
 }
