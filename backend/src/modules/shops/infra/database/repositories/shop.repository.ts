e5ee0034d17e6f19630cca9b46f7d19f33c7c88d
Vinícius <buenov535@gmail.com>
2026-03-03 21:53:35 -0300

    feat(shop): implemented shop repository save function

---
+++ backend/src/modules/shops/infra/database/repositories/shop.repository.ts
@@ -0,0 +1,18 @@
+import { ShopEntity } from "@/modules/shops/domain/entities/shop.entity";
+import { ShopRepository } from "@/modules/shops/domain/repositories/shop.repository";
+import { PrismaClient } from "@prisma-generated/client";
+import { ShopMapper } from "../../mappers/shop.mapper";
+
+export class PrismaShopRepository implements ShopRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async save(shop: ShopEntity): Promise<void> {
+    const raw = ShopMapper.toPersistence(shop);
+
+    await this.prisma.shop.create({
+      data: raw,
+    });
+
+    return;
+  }
+}
