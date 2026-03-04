8ba0b9675cf12af80f6de89d2af170c7a01b33c8
Vinícius <buenov535@gmail.com>
2026-03-03 22:40:37 -0300

    feat(auth): added authenticated user to context

--- backend/src/routes/graphql.ts
+++ backend/src/routes/graphql.ts
@@ -2,12 +2,19 @@ import { Router } from "express";
 import { createHandler } from "graphql-http/lib/use/express";
 import schema from "@infra/graphql/schema";
 import { container } from "@infra/container";
+import { getAuthenticatedUser } from "@/infra/graphql/middlewares/auth.middleware";
 
 const graphqlRoutes = Router();
 
 graphqlRoutes.all(
   "/graphql",
-  createHandler({ schema, context: { container } }),
+  createHandler({
+    schema,
+    context: ({ raw }) => ({
+      container,
+      user: getAuthenticatedUser(raw.headers.authorization),
+    }),
+  }),
 );
 
 export default graphqlRoutes;
