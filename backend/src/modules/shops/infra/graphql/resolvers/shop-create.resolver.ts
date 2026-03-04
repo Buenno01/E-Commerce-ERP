967d64d530c9920af6927bb3e23c82c8e2282c55
Vinícius <buenov535@gmail.com>
2026-03-03 23:05:34 -0300

    feat(shops): shop create resolver now inserts the user in context into the input

--- backend/src/modules/shops/infra/graphql/resolvers/shop-create.resolver.ts
+++ backend/src/modules/shops/infra/graphql/resolvers/shop-create.resolver.ts
@@ -1,6 +1,11 @@
 import { ShopCreateUseCase } from "@/modules/shops/application/use-cases/shop-create.use-case";
 
 export default function shopCreate(_: any, { input }: any, context: any) {
+  if (!context.user) {
+    throw new Error(
+      "Forbidden: you must be authenticated in order to create a Shop",
+    );
+  }
   const useCase = context.container.resolve(ShopCreateUseCase);
-  return useCase.execute(input);
+  return useCase.execute({ ...input, ownerId: context.user.sub });
 }
